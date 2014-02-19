1a1e0164f7cb488026d8dd4c1b9e81a81306b93a
Chris <veilofsuperstition@gmail.com>
2014-02-19 10:12:06 +0100

    hey

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Permissions;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -40,16 +41,28 @@ namespace Findsmallest
 
         static void Main(string[] args)
         {
+            List < Task<int> > tasklist= new List<Task<int>>();
             foreach (int[] data in Data)
             {
                 //int smallest = FindSmallest(data);
                 //Thread t1 = new Thread(()=> FindSmallest(data));
                 Task<int> task1 = Task<int>.Factory.StartNew(() => FindSmallest(data));
                 //Task.Factory.StartNew(() => FindSmallest(data));
-                Console.WriteLine(task1.Result);
+                tasklist.Add(task1);
                 
                 //Console.WriteLine(String.Join(", ", data) + ": " + smallestSoFar);
             }
+            
+                foreach (Task<int> task in tasklist)
+                {
+                        if (task.Result != null)
+                        {
+                            Console.WriteLine(task.Result);
+                           
+                        }
+                }
+            
+            
             Console.ReadLine();
         }
     }
